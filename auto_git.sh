cb3d5e88b0749e5202d2f6c292072528072378e0
Kyokoning <853230423@qq.com>
2020-09-29 10:22:04 +0100

    auto_update

--- auto_git.sh
+++ auto_git.sh
@@ -2,12 +2,11 @@ echo `date +"%Y-%m-%d %H:%M:%S"`
 echo "####### 进入项目路径 #######"
 /home/xnchen/code/save_page_for_99
 echo "####### 开始自动备份 #######" 
-git add .
+/usr/bin/git add .
 
 sleep 1s
-git commit -m "auto_update"
+/usr/bin/git commit -m "auto_update"
 sleep 1s
-git push -u origin master
+/usr/bin/git push -u origin master
 
 echo "####### 自动备份执行完毕 #######"
-
