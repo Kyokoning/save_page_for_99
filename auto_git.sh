b2724e9c995a830adc290bbff0e8b6f2fc9634d3
Kyokoning <853230423@qq.com>
2020-09-29 11:12:01 +0100

    提交的时间是: 2020年 09月 29日 星期二 11:12:01 WEST

--- auto_git.sh
+++ auto_git.sh
@@ -5,4 +5,4 @@ ${GIT} add --all
 ${GIT} pull --rebase
 time=`date`
 ${GIT} commit -m "提交的时间是: $time"
-HOME=/home/xnchen ${GIT} push origin master:server_backup
+HOME=/home/xnchen ${GIT} push origin server_backup
