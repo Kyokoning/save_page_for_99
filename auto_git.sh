f8fc1baba9bc591e077364182897a9291334e7cf
Kyokoning <853230423@qq.com>
2020-09-29 09:33:05 +0100

    2020-09-29 09:33:05

---
+++ auto_git.sh
@@ -0,0 +1,13 @@
+echo "####### 进入项目路径 #######"
+/home/xnchen/code/save_page_for_99
+echo "####### 开始自动备份 #######" 
+nohup git pull >> git.log 2>&1 &
+nohup git add . >> git.log 2>&1 &
+
+sleep 1s
+nohup git commit -m "`date +"%Y-%m-%d %H:%M:%S"`" >> git.log 2>&1 &
+sleep 1s
+nohup git push -u origin master >> git.log 2>&1 &
+
+echo "####### 自动备份执行完毕 #######"
+
