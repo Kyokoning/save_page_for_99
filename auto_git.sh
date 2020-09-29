6da0b182c189a8a49bec28b7f70aad3eb6164c39
Kyokoning <853230423@qq.com>
2020-09-29 09:50:36 +0100

    auto_update

--- auto_git.sh
+++ auto_git.sh
@@ -7,6 +7,8 @@ nohup git add . >> git.log 2>&1 &
 sleep 1s
 nohup git commit -m "auto_update" >> git.log 2>&1 &
 sleep 1s
+nohup git pull --rebase >> git.log 2>&1 &
+sleep 5s
 nohup git push -u origin master >> git.log 2>&1 &
 
 echo "####### 自动备份执行完毕 #######"
